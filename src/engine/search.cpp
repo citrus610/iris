78fc31a011e5de7ab31056ab551030d7284f2988
citrus610 <79373642+citrus610@users.noreply.github.com>
2025-06-09 00:57:17 +0700

    razoring
    bench: 10727086

--- src/engine/search.cpp
+++ src/engine/search.cpp
@@ -326,6 +326,17 @@ i32 Engine::pvsearch(Data& data, i32 alpha, i32 beta, i32 depth)
         is_improving = data.stack[data.ply].eval > data.stack[data.ply - 2].eval;
     }
 
+    // Razoring
+    if (!PV &&
+        depth <= tune::RAZOR_DEPTH &&
+        eval + tune::RAZOR_COEF * depth < alpha) {
+        i32 score = this->qsearch<false>(data, alpha, beta);
+
+        if (score <= alpha) {
+            return score;
+        }
+    }
+
     // Reverse futility pruning
     if (!PV &&
         depth <= tune::RFP_DEPTH &&
