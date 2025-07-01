a83e0c48aec20cee39ae077acc70243ad96480d6
citrus610 <79373642+citrus610@users.noreply.github.com>
2025-07-01 23:01:18 +0700

    misc: index history by move hash
    non functional
    bench: 6953431

--- src/engine/history.cpp
+++ src/engine/history.cpp
@@ -9,7 +9,7 @@ i16& Table::get(Board& board, const u16& move)
     assert(square::is_valid(move::get_from(move)));
     assert(square::is_valid(move::get_to(move)));
 
-    return this->data[board.get_color()][move::get_from(move)][move::get_to(move)];
+    return this->data[board.get_color()][move & 0xFFF];
 };
 
 void Table::update(Board& board, const u16& move, i16 bonus)
