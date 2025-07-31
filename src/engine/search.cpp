da563a1231a71c658fc26ee17fedc9e0aba92d61
citrus610 <79373642+citrus610@users.noreply.github.com>
2025-07-31 14:53:25 +0000

    more reduction if we're behind nmp
    bench: 5489158

--- src/engine/search.cpp
+++ src/engine/search.cpp
@@ -588,6 +588,7 @@ i32 Engine::pvsearch(Data& data, i32 alpha, i32 beta, i32 depth, bool is_cut)
             reduction -= move == data.stack[data.ply - 1].killer;
             reduction += !is_improving;
             reduction += is_cut;
+            reduction += data.ply >= 2 && data.stack[data.ply - 2].move == move::NONE;
 
             // Clamps depth to avoid qsearch
             i32 depth_reduced = std::min(std::max(depth_next - reduction, 1), depth_next);
