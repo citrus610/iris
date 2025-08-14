7b4ac59061846a3d9ef77688f5e23d29c2500b0d
citrus610 <79373642+citrus610@users.noreply.github.com>
2025-08-14 04:30:04 +0000

    forgot about qsearch
    bench: 3310725

--- src/engine/search.cpp
+++ src/engine/search.cpp
@@ -956,11 +956,13 @@ i32 Engine::qsearch(Data& data, i32 alpha, i32 beta)
         // Updates best
         if (score > best) {
             best = score;
-            best_move = move;
 
             // Updates alpha
             if (score > alpha) {
                 alpha = score;
+
+                // Updates best move
+                best_move = move;
     
                 // Updates pv line
                 data.stack[data.ply].pv.update(move, data.stack[data.ply + 1].pv);
